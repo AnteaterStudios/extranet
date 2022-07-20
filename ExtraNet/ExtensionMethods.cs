e3edcf94817ca44cfe3cc6b051120b2892eb0a5a
Anteater Studios <anteaterstudios.games@gmail.com>
2022-07-20 11:14:50 -0300

    Add extension method for array
    T[] Set<T>: set data starting from index

--- ExtraNet/ExtensionMethods.cs
+++ ExtraNet/ExtensionMethods.cs
@@ -1,6 +1,12 @@
 ﻿namespace AnteaterStudios.ExtraNet.ExtensionMethods;
 public static class ExtensionMethods
 {
+    public static T[] Set<T>(this T[] self, int index, params T[] values)
+    {
+        Array.Copy(values, 0, self, index, values.Length);
+        return self;
+    }
+
     public static bool HasDefaultConstructor(this Type t)
     {
         return t.IsValueType || t.GetConstructor(Type.EmptyTypes) != null;
